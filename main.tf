e8eceda9cc6f31b2ccaaf66b667e9c365d7e4be2
Kyler Dana <kdana@virtra.local>
2025-09-12 14:42:17 -0700

    Added public access block and ownership lock for S3 Bucket to main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,24 @@ resource "aws_s3_bucket" "this" {
   tags = local.tags
 }
 
+# Block all public access (default)
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id 
+  block_public_acls = true 
+  block_public_policy = true 
+  ignore_public_acls = true 
+  restrict_public_buckets = true 
+}
+
+# For Access Control Lists in some regions, keeps same ownership
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id 
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+
+
+
+
