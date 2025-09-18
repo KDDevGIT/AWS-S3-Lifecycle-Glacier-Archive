f7b0f27708a07480d340aca0232d6da418ea08b1
Kyler Dana <kdana@virtra.local>
2025-09-18 15:19:23 -0700

    Added s3 bucket versioning and policy document to main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,64 @@ resource "aws_s3_bucket_ownership_controls" "this" {
   }
 }
 
+# Default Encryption (AES256) 
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
+# Versioning for Robust Lifecycle Rules
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id 
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enforce TLS-Only
+# Deny unencrypted uploads
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    sid = "DenyInsecureTransport"
+    effect = "Deny"
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.this.arn, 
+      "${aws_s3_bucket.this.arn}/*"
+    ]
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test = "Bool"
+      variable = "aws:SecureTransport"
+      values = ["false"]
+    }
+  }
+
+  statement {
+    sid = "DenyUnEncryptedObjectUploads"
+    effect = "Deny"
+    actions = ["s3:PutObject"]
+    resources = [
+      "${aws_s3_bucket.this.arn}/*"
+    ]
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+      values = ["AES256", "aws:kms"]
+    }
+  }
+}
 
 
 
